1f01de334035e8b27a907db0217e4211f82268ab
abdullah-dev-11804 <abdullahamin9190212gmail.com>
2026-09-11 03:15:18 +0500

    version

--- version.php
+++ version.php
@@ -12,10 +12,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'quizaccess_proctorcore';
-$plugin->version = 2026091100;
+$plugin->version = 2026091101;
 $plugin->requires = 2022041900; // Moodle 4.0+.
 $plugin->dependencies = [
-    'local_proctorcore' => 2026091100,
+    'local_proctorcore' => 2026091001,
 ];
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = '0.13.0 - Candidate experience refresh';
